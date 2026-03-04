4eeea7156bef257c21354b6ab7149ae36ac23692
ricardocotillo <ricardo.cotillo@gmail.com>
2026-03-03 20:52:33 -0500

    refactor: remove unused go_router import from payment success screen

--- lib/screens/payment_success_screen.dart
+++ lib/screens/payment_success_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 import 'package:proplay/bloc/credit/credit_bloc.dart';
 import 'package:proplay/bloc/credit/credit_event.dart';
 import 'package:proplay/bloc/credit/credit_state.dart';
