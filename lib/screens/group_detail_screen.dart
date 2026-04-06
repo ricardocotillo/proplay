409c646a9098f8ba64ad2b0483004f2708e44604
ricardocotillo <ricardo.cotillo@gmail.com>
2026-04-06 10:49:52 -0500

    refactor: replace NetworkImage with CachedNetworkImageProvider for member profile images
    - Add cached_network_image package import
    - Replace NetworkImage with CachedNetworkImageProvider in group member list tiles

--- lib/screens/group_detail_screen.dart
+++ lib/screens/group_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:proplay/bloc/group_detail/group_detail_bloc.dart';
 import 'package:proplay/bloc/group_detail/group_detail_event.dart';
 import 'package:proplay/bloc/group_detail/group_detail_state.dart';
@@ -205,7 +206,7 @@ class GroupDetailScreen extends StatelessWidget {
         return ListTile(
           leading: CircleAvatar(
             backgroundImage: member.user.profileImageUrl != null
-                ? NetworkImage(member.user.profileImageUrl!)
+                ? CachedNetworkImageProvider(member.user.profileImageUrl!)
                 : null,
             child: member.user.profileImageUrl == null
                 ? const Icon(Icons.person)
